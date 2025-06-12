c87d3d2bca2ea0f8339171572ce38872d2f6023a
Somyayadu <honeyyaduvanshi496@gmail.com>
2025-06-12 09:46:21 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,41 @@
+                                               #string
+
+# name="somya"
+# print (name)
+# print (type(name))
+
+#name="somyayaduvanshi"
+# print (name.upper())
+# print(name.lower())
+# print(name.title())
+# print(name.capitalize())
+# print(name.casefold())
+# print(name.swapcase())
+# print(name.strip())
+# print(name.lstrip())
+# print(name.rstrip())
+# print(name.replace("yaduvanshi","yadav"))
+# print(name.split(" "))
+# print(name.partition(" "))
+# print(name.startswith("s"))
+# print(name.endswith("i"))
+# print(name.isalpha())
+# print(name.isalnum())
+# print(name.encode())
+# print(name.removeprefix("somya"))
+# print(name.zfill(20))
+# print(name.center(30,"*"))
+
+# name ="somya yaduvanshi"
+# print(name[0])
+# print(name[-1])
+# print(name[0:5])
+
+# age=10
+# text=f"my age is {age}"
+# print (text)
+
+# text= "my name is \"jai\""
+# print(text)
+
+                                             
